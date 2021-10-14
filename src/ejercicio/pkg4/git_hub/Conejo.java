afcf438f3ca95587762f051cd3712fc5423dc797
jnfers <jnfers@hotmail.com>
2021-10-14 12:42:18 -0300

    RamaFer

---
+++ src/ejercicio/pkg4/git_hub/Conejo.java
@@ -0,0 +1,15 @@
+
+package ejercicio.pkg4.git_hub;
+
+
+public class Conejo {
+    
+    private String nombre;
+    private String color;
+    private Double peso;
+    private final String Raza="Angora";
+    
+    
+    
+    
+}
