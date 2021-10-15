0b2f3ca4a4bc3a374a48c6a86c6361959a369674
Gianfranco Ballester <gianballester@gmail.com>
2021-10-15 12:49:01 -0300

    Perro

---
+++ src/ejercicio/pkg4/git_hub/Perro.java
@@ -0,0 +1,12 @@
+
+package ejercicio.pkg4.git_hub;
+
+
+public class Perro {
+    
+    private String nombre;
+    private int edad;
+    private String color;
+    private String raza;
+    private Double peso;
+}
